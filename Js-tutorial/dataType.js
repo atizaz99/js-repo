369043e9ed14c662556719649ef97c6ccced70d7
atizaz99 <atizazhussain1412@gmail.com>
2025-12-26 01:31:28 +0500

    The Data type of js

---
+++ Js-tutorial/dataType.js
@@ -0,0 +1,21 @@
+let ans = 2345;
+console.log(ans);
+ans = "This is my answer";
+console.log(ans);
+
+/*
+    Number and and the '+' Operator
+*/
+x = 'The Number after ' + 42;
+console.log(x)
+y = 545 + ' number after string '
+console.log(y)
+
+z = '77'+9
+console.log(z)
+
+/*
+    other operator not perform cancad
+*/
+console.log('77'-9)
+console.log('99'*6)
