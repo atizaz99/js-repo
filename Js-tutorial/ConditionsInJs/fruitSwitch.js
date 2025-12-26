e64e005669d177e9190a0898e40ee0f4b71070ce
atizaz99 <atizazhussain1412@gmail.com>
2025-12-26 21:47:09 +0500

    The switch statement of js use for  condition
    2

---
+++ Js-tutorial/ConditionsInJs/fruitSwitch.js
@@ -0,0 +1,16 @@
+const fruitType = 'Oranges';
+
+switch(fruitType){
+    case "Oranges":
+        console.log(`${fruitType} are $0.059 per pound.`);
+        break;
+
+    case 'Bananas':
+        console.log(`${fruitType} are $0.09 per pound.`);
+        break;
+
+    default:
+        console.log(`Sorry, We are out of ${fruitType}.`);
+        break;
+}
+
