83baf14b8cbed1b8bf06221782b84ddaaaca37f8
atizaz99 <atizazhussain1412@gmail.com>
2026-01-05 03:11:43 +0500

    the

---
+++ Js-tutorial/objects/01_object1.js
@@ -0,0 +1,23 @@
+// singleton object: Singleton object defined by a constructor
+// Object.create
+
+// object literal 
+
+const jsUser = {
+    name: "Alice",
+    age: 30,
+    loaction: "WonderLand",
+    email: "alice@wornderland",
+    isLoggedIn: true,
+    lastLoginDays: ["Monday", "Friday"],
+    "full name": "Alice Johnson" // key with space
+
+
+}
+
+// object accessing
+console.log(jsUser.name); // using dot notation 
+console.log(jsUser["age"]); // using bracket notation
+console.log(jsUser["full name"]); // using bracket notation for key with spaces
+
+
